63f9ef8ee401b0e27c2f45abbcd6b30a8c8d415c
Dmytro Zelenyi <dzeleniy9@gmail.com>
2020-10-16 22:04:54 +0300

    Replace files

--- blogengine/asgi.py
+++ blogengine/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for DjangoBlog project.
+ASGI config for blogengine project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoBlog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogengine.settings')
 
 application = get_asgi_application()
