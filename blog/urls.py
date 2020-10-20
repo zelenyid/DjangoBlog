02c0998457924201ca546fec21d08d0864b8c9dd
Dmytro Zelenyi <dzeleniy9@gmail.com>
2020-10-20 20:29:05 +0300

    Add post list and post details

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', posts_list)
+    path('', posts_list, name='posts_list_url'),
+    path('post/<str:slug>/', post_detail, name='post_detail_url')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 
 def posts_list(request):
-    n = ['Dmytro', 'Masha', 'Olga', 'Kolya']
-    return render(request, 'blog/index.html', context={'names': n})
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={'posts': posts})
+
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug__iexact=slug)
+    return render(request, 'blog/post_detail.html', context={'post': post})
