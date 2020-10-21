a33a23724137689862b528fe09aebb6709d0611a
Dmytro Zelenyi <dzeleniy9@gmail.com>
2020-10-21 15:56:34 +0300

    Add tags to blog

--- blog/models.py
+++ blog/models.py
@@ -8,8 +8,21 @@ class Post(models.Model):
     body = models.TextField(blank=True, db_index=True)
     data_pub = models.DateTimeField(auto_now_add=True)
 
+    tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
+
     def get_absolute_url(self):
         return reverse('post_detail_url', kwargs={'slug': self.slug})
+
+    def __str__(self):
+        return '{}'.format(self.title)
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def get_absolute_url(self):
+        return reverse('tag_detail_url', kwargs={'slug': self.slug})
 
     def __str__(self):
         return '{}'.format(self.title)
