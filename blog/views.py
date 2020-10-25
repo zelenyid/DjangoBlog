843422cbfa2997d58e92045d40e27677ea40b8a4
Dmytro Zelenyi <dzeleniy9@gmail.com>
2020-10-25 17:55:42 +0200

    Add class PostCreate and template form for it

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import get_object_or_404
 from django.views.generic import View
 
 from .models import Post, Tag
-from .utils import ObjectDetailMixin
-from .forms import TagForm
+from .utils import *
+from .forms import TagForm, PostForm
 
 
 def posts_list(request):
@@ -18,24 +18,19 @@ class PostDetail(ObjectDetailMixin, View):
     template = 'blog/post_detail.html'
 
 
+class PostCreate(ObjectCreateMixin, View):
+    form_model = PostForm
+    template = 'blog/post_create_form.html'
+
+
 class TagDetail(ObjectDetailMixin, View):
     model = Tag
     template = 'blog/tag_detail.html'
 
 
-class TagCreate(View):
-    def get(self, request):
-        form = TagForm
-        return render(request, 'blog/tag_create.html', context={'form': form})
-
-    def post(self, request):
-        bound_form = TagForm(request.POST)
-
-        if bound_form.is_valid():
-            new_tag = bound_form.save()
-            return redirect(new_tag)
-
-        return render(request, 'blog/tag_create.html', context={'form': bound_form})
+class TagCreate(ObjectCreateMixin, View):
+    form_model = TagForm
+    template = 'blog/tag_create.html'
 
 
 def tags_list(request):
