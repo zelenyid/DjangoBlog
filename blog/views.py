371bf8a0a255dfcf821ee8b5c4f1841e0e2e795f
Dmytro Zelenyi <dzeleniy9@gmail.com>
2020-11-01 20:37:02 +0200

    Add search by header and body in posts

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 from .models import Post, Tag
 from .utils import *
@@ -12,7 +13,13 @@ from .forms import TagForm, PostForm
 
 
 def posts_list(request):
-    posts = Post.objects.all()
+    search_query = request.GET.get('search', '')
+
+    if search_query:
+        posts = Post.objects.filter(Q(title__icontains=search_query) | Q(body__icontains=search_query))
+    else:
+        posts = Post.objects.all()
+
     paginator = Paginator(posts, 10)
 
     page_number = request.GET.get('page', 1)
